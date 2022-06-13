9a910469b680a43706d48547a826dc88c0810b2e
chenweiqiang <464306572@qq.com>
2022-06-13 15:46:09 +0800

    build: add submodule DiContainer

--- RESTful/build.gradle.kts
+++ RESTful/build.gradle.kts
@@ -13,9 +13,9 @@ dependencies {
     implementation("jakarta.inject:jakarta.inject-api:2.0.1.MR")
     implementation("jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
     implementation("jakarta.servlet:jakarta.servlet-api:5.0.0")
-    implementation(project(":01.di.container"))
+    implementation(project(":DiContainer"))
     testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
-    testImplementation("org.mockito:mockito-core:4.5.1")
+    testImplementation("org.mockito:mockito-core:4.6.1")
     testImplementation("org.eclipse.jetty:jetty-server:11.0.9")
     testImplementation("org.eclipse.jetty:jetty-servlet:11.0.9")
 
