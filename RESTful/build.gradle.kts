8148d7619ed31d18c16cc53efd655713ebdc3d82
chenweiqiang <464306572@qq.com>
2022-06-13 14:53:17 +0800

    Initial commit

---
+++ RESTful/build.gradle.kts
@@ -0,0 +1,29 @@
+
+plugins {
+    `java-library`
+    jacoco
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+repositories {
+    mavenCentral()
+}
+dependencies {
+    implementation("jakarta.inject:jakarta.inject-api:2.0.1.MR")
+    implementation("jakarta.ws.rs:jakarta.ws.rs-api:3.1.0")
+    implementation("jakarta.servlet:jakarta.servlet-api:5.0.0")
+    implementation(project(":01.di.container"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.8.2")
+    testImplementation("org.mockito:mockito-core:4.5.1")
+    testImplementation("org.eclipse.jetty:jetty-server:11.0.9")
+    testImplementation("org.eclipse.jetty:jetty-servlet:11.0.9")
+
+}
+tasks.withType<Test>() {
+    useJUnitPlatform()
+}
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}
